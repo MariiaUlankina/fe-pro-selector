0b360b7f7f6b7549681556f100f57a6479058396
MariiaUlankina <ulankina.masha@gmail.com>
2021-12-29 11:21:03 +0200

    added function selector

--- src/selector.js
+++ src/selector.js
@@ -1,5 +1,18 @@
 const selector = (obj, keys) => {
     // Ваше решение должно быть здесь
+
+    let newObj = obj;
+
+    keys.forEach((item) => {
+        if(newObj[item]){
+            newObj = newObj[item];
+
+        }
+        else newObj = ' ';  
+        
+    });
+
+    return newObj;
 };
 
 module.exports = selector;
